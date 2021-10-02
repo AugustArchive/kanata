e36461e86453df9c050378c44d7397adbaf0edfd
Noel <cutie@floofy.dev>
2021-10-02 16:58:22 -0700

    Initial project

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# Kanata
-> 💫 **Small microservice to handle state changes of Kubernetes pods and post them to Instatus or Statuspages**
+# 💫 Kanata
+> **Small microservice to handle state changes of Kubernetes pods and post them to Instatus or Statuspages**
+
+## 🤔 Why?
+I don't really want to implement and repeat code to report the pod's state on Instatus on every project I make,
+so I made it as a small, scalable microservice that shouldn't be down most of the time unless the Kubernetes
+cluster needs to restart.
+
+The name is inspired by one my favourite vtubers I like to watch, [Amane Kanata](https://www.youtube.com/channel/UCZlDXzGoo7d44bwdNObFacg).
+
+## 🖥️ Installation
+You can install Kanata using your own machine or with Docker! Kanata
+is meant supposed to be small, and easy to use. All you really need installed
+is JDK 16.
+
+I provide a Docker image over at [Docker Hub](https://hub.docker.com/-/noelware/kanata) if you want to run
+Kanata in a Docker container.
+
+### Docker
+All you really need to do is:
+
+```shell
+# 1. Pull the image off of Docker Hub
+$ docker pull noelware/kanata:latest # you can also specify a version or commit hash
+
+# 2. Run it!
+$ docker run -d -p <host>:22903 --name kanata --restart always \
+  -v /path/to/config:/app/noelware/kanata/config.yml \
+  noelware/kanata:latest # or specify the version / hash you used
+```
+
+### Git Repository
+You will need the Git command tools installed before proceeding.
+
+```shell
+# 1. Clone the repository
+$ git clone https://github.com/auguwu/Kanata && cd Kanata
+
+# 2. Run `build` to build the JAR file.
+$ ./gradlew build # or `.\gradlew` on PowerShell or `gradlew` in CMD
+
+# 3. Run the jar file!
+$ java -jar ./build/libs/Kanata.jar
+```
+
+## 🪦 Providers
+Kanata monitors any Kubernetes pod available, so it is assured that Kanata
+supports many providers like [Instatus](https://instatus.com).
+
+To create your own provider, you can add a provider by contributing! A basic provider
+would be like:
+
+```kotlin
+import dev.floofy.kanata.providers.IProvider
+import dev.floofy.kanata.Kanata
+
+class MyProvider(kanata: Kanata): IProvider(kanata) {
+    override val name: String = "my:provider:name"
+    override suspend fun dispatch(data: StatusPacket) {
+        // dispatched when we receive a status update.
+    }
+}
+```
+
+And submit a [Pull Request!](https://github.com/auguwu/Kanata/compare)~
+
+## ⚡ License
+**Kanata** is released under the **GPL-3.0** License, read [here](/LICENSE) for more information!
---
+++ build.gradle.kts
@@ -0,0 +1,172 @@
+/**
+ * Microservice to handle Kubernetes pod state changes and reports them in Instatus or Statuspages.
+ * Copyright (c) 2021 Noel <cutie@floofy.dev>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import java.io.File
+import java.util.concurrent.TimeUnit
+import java.util.Date
+import java.text.SimpleDateFormat
+
+buildscript {
+    repositories {
+        mavenCentral()
+        mavenLocal()
+        jcenter()
+    }
+}
+
+plugins {
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+    kotlin("plugin.serialization") version "1.5.31"
+    id("com.diffplug.spotless") version "5.14.0"
+    kotlin("jvm") version "1.5.31"
+    application
+}
+
+val current = Version(1, 0, 0)
+group = "dev.floofy.services"
+version = current.string()
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+    jcenter()
+    maven {
+        url = uri("https://maven.floofy.dev/repo/releases")
+    }
+}
+
+dependencies {
+    // Kotlin Libraries
+    implementation(kotlin("stdlib", "1.5.31"))
+
+    // Ktor (server, http)
+    implementation("io.ktor:ktor-client-serialization:1.6.3")
+    implementation("io.ktor:ktor-serialization:1.6.3")
+    implementation("io.ktor:ktor-client-okhttp:1.6.3")
+    implementation("io.ktor:ktor-server-netty:1.6.3")
+
+    // Kotlinx.Serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
+    api("org.jetbrains.kotlinx:kotlinx-serialization-core:1.2.1")
+
+    // Logging (slf4j + logback)
+    implementation("ch.qos.logback:logback-classic:1.2.6")
+    implementation("ch.qos.logback:logback-core:1.2.6")
+    api("org.slf4j:slf4j-api:1.7.32")
+
+    // Kubernetes API
+    implementation("io.kubernetes:client-java:13.0.0")
+
+    // YAML
+    implementation("com.charleskorn.kaml:kaml:0.36.0")
+
+    // Haru (cron scheduler)
+    implementation("dev.floofy.haru:Haru:1.2.0")
+}
+
+tasks.register("generateMetadata") {
+    val path = sourceSets["main"].resources.srcDirs.first()
+    if (!file(path).exists()) path.mkdirs()
+
+    val date = Date()
+    val formatter = SimpleDateFormat("MMM dd, YYYY - HH:mm:ss")
+
+    file("$path/metadata.properties").writeText("""app.version = ${current.string()}
+app.commit  = ${current.commit()}
+app.build.date = ${formatter.format(date)}
+""".trimIndent())
+}
+
+spotless {
+    kotlin {
+        trimTrailingWhitespace()
+        licenseHeaderFile("${rootProject.projectDir}/assets/HEADING")
+        endWithNewline()
+
+        // We can't use the .editorconfig file, so we'll have to specify it here
+        // issue: https://github.com/diffplug/spotless/issues/142
+        // ktlint 0.35.0 (default for Spotless) doesn't support trailing commas
+        ktlint("0.40.0")
+            .userData(mapOf(
+                "no-consecutive-blank-lines" to "true",
+                "no-unit-return" to "true",
+                "disabled_rules" to "no-wildcard-imports,colon-spacing",
+                "indent_size" to "4"
+            ))
+    }
+}
+
+application {
+    mainClass.set("dev.floofy.discord.noel.Bootstrap")
+    java {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = JavaVersion.VERSION_11.toString()
+        kotlinOptions.javaParameters = true
+        kotlinOptions.freeCompilerArgs += listOf(
+            "-Xopt-in=kotlin.RequiresOptIn"
+        )
+    }
+
+    named<ShadowJar>("shadowJar") {
+        archiveFileName.set("Noel.jar")
+        mergeServiceFiles()
+
+        manifest {
+            attributes(mapOf(
+                "Manifest-Version" to "1.0.0",
+                "Main-Class" to "dev.floofy.discord.noel.Bootstrap"
+            ))
+        }
+    }
+
+    build {
+        dependsOn("generateMetadata")
+        dependsOn(spotlessApply)
+        dependsOn(shadowJar)
+    }
+}
+
+class Version(
+    private val major: Int,
+    private val minor: Int,
+    private val patch: Int
+) {
+    fun string(): String = "$major.$minor.$patch"
+    fun commit(): String = execShell("git rev-parse HEAD")
+}
+
+fun execShell(command: String): String {
+    val parts = command.split("\\s".toRegex())
+    val process = ProcessBuilder(*parts.toTypedArray())
+        .directory(File("."))
+        .redirectOutput(ProcessBuilder.Redirect.PIPE)
+        .redirectError(ProcessBuilder.Redirect.PIPE)
+        .start()
+
+    process.waitFor(1, TimeUnit.MINUTES)
+    return process.inputStream.bufferedReader().readText()
+        .trim()
+        .slice(0..8)
+}
